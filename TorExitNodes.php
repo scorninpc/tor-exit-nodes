af46883044684279246e48a97262684344468d93
Cleberson Falk <cleberson.falk@gmail.com>
2016-06-17 12:08:16 -0300

    Criação dos comentários e README.md

--- TorExitNodes.php
+++ TorExitNodes.php
@@ -1,17 +1,32 @@
 <?php
 
+/**
+ * Classe que disponibiliza um mecanismo de identificação de IPs
+ * que tem como origem a rede TOR.
+ */
 class TorExitNodes
 {
     private $exit_addresses_url = 'https://check.torproject.org/exit-addresses';
     private $exit_addresses_file = 'exit_addresses.txt';
     private $update_interval = 4; // hours
 
+    /**
+     * Método que faz o download de todas as informações
+     * a respeito dos exit nodes.
+     *
+     * @return string
+     */
     public function loadExitAddresses()
     {
-        $exit_addresses = file_get_contents($this->exit_addresses_url);
-        return $exit_addresses;
+        return file_get_contents($this->exit_addresses_url);
     }
 
+    /**
+     * Método que manipula o texto bruto disponível na página dos exit addresses
+     * e extrai os IPs, depois armazena tudo em um arquivo TXT.
+     *
+     * @return void
+     */
     public function updateExitAddressIps()
     {
         $exit_addresses = $this->loadExitAddresses();
@@ -23,6 +38,13 @@ class TorExitNodes
         }
     }
 
+    /**
+     * Método responsável por verificar o tempo da última atualização dos IPs,
+     * caso a última atualização for a mais de 4 horas, executa a
+     * atualização dos IPs novamente.
+     *
+     * @return void
+     */
     public function checkFileModifiedTime()
     {
         $file_modified_time = filemtime($this->exit_addresses_file);
@@ -30,12 +52,18 @@ class TorExitNodes
         $date_mfile = new Datetime(date('Y-m-d H:i:s', $file_modified_time));
         $date_now = new Datetime('now');
 
-        // if the file was modified more than 4 hours run update.
+        // se o arquivo foi modificado a mais de 4 horas, executa o update.
         if($date_mfile->diff($date_now)->h >= $this->update_interval) {
             $this->updateExitAddressIps();
         }
     }
 
+    /**
+     * Método que verifica se um determinado IP é um exit node da rede TOR.
+     *
+     * @param string $ip
+     * @return bool
+     */
     public function isTorExitNode($ip)
     {
         $this->checkFileModifiedTime();
